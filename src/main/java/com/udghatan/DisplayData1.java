7498357bfb17f570c2faf52ba4cda822508b51b1
uvesnoida <uvesnoida@gmail.com>
2017-07-23 01:29:49 +0530

    Add files via upload

---
+++ src/main/java/com/udghatan/DisplayData1.java
@@ -0,0 +1,25 @@
+package com.udghatan;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DisplayData1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("username");
+		out.println("The name is "+name);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World! Chal gaya yeh finally :)</h2>
+
+<form action="DisplayData1" method="get">
+<input type="text" name="username">
+<input type="submit" value="Go to">
+</form>
+</body>
+</html>
